497f09b3481cf050c22304b50bb54aacdf92adce
root <root@DESKTOP-6TN6PTL.localdomain>
2024-03-26 22:25:29 +0800

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :date_of_birth, presence: true
-
   validate :date_of_birth_format
+
+  has_many :articles, dependent: :nullify
+  has_many :comments, dependent: :nullify
 
   private
 
