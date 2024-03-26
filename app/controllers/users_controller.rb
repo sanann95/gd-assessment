bbef19e13aa0e12faa38c7722567a59a41573515
root <root@DESKTOP-6TN6PTL.localdomain>
2024-03-26 15:57:58 +0800

    Requirement 2 & 3

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,61 @@
+class UsersController < ApplicationController
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+	  @user = User.find(params[:id])
+	  if @user.update(user_params)
+	    redirect_to @user, notice: 'User was successfully updated.'
+	  else
+	    render :edit
+	  end
+	end
+
+  # DELETE /users/1
+  def destroy
+    @article = User.find(params[:id])
+    @article.destroy
+
+    redirect_to users_url
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:name, :email, :date_of_birth)
+    end
+end
